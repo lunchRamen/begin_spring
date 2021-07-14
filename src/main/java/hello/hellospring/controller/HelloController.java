30c9442fcde97b68946c0e878dc75ed22cc22768
lunchRamen <bloom6561@naver.com>
2021-07-15 04:05:22 +0900

    first practice

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String Hello(Model model){
-        model.addAttribute("data","Hello!");
+        model.addAttribute("data","Hello!!");
         return "hello";
     }
 }
